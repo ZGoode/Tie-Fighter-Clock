05fe83e41cfe42b41021540e080c17f251ffa6f2
Zachary Nathan Goode <zachary@the-goodes.com>
2020-04-12 10:46:14 -0700

    update

--- Source/Tie-Fighter-Clock/Tie-Fighter-Clock.ino
+++ Source/Tie-Fighter-Clock/Tie-Fighter-Clock.ino
@@ -1,46 +1,244 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <WiFiClient.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "FS.h"
+
 #include "Empire.h"
+#include "HTML.h"
 #include "Vader.h"
-#include "Settings.h"
+
+#define VERSION "1.0"
+#define HOSTNAME "Tie-Fighter-Clock"
+#define CONFIG "/conf.txt"
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define INVERT_DISPLAY false
+
+const int WEBSERVER_PORT = 80;
+const boolean WEBSERVER_ENABLED = true;
+char* www_username = "admin";
+char* www_password = "password";
+
+boolean ENABLE_OTA = true;
+String OTA_Password = "";
 
 ESP8266WebServer server(WEBSERVER_PORT);
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
 
 WiFiUDP ntpUDP;
 
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
 
-const int externalLight = externalLight_BUILTIN;
+const int externalLight = LED_BUILTIN;
 
 void setup(void) {
   Serial.begin(115200);
   SPIFFS.begin();
   delay(10);
 
+  Serial.println();
+  pinMode(externalLight, OUTPUT);
+
+  readSettings();
+
+  display.display();
+  if (INVERT_DISPLAY) {
+    display.invertDisplay(true); // connections at top of OLED display
+  }
+
+  display.clearDisplay();
+  display.display();
+
+  parseHomePage();
+  parseConfigurePage();
+
+  WiFiManager wifiManager;
+
+  String hostname(HOSTNAME);
+  hostname += String(ESP.getChipId(), HEX);
+  if (!wifiManager.autoConnect((const char *)hostname.c_str())) {// new addition
+    delay(3000);
+    WiFi.disconnect(true);
+    ESP.reset();
+    delay(5000);
+  }
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.setHostname((const char *)hostname.c_str());
+  if (OTA_Password != "") {
+    ArduinoOTA.setPassword(((const char *)OTA_Password.c_str()));
+  }
+  ArduinoOTA.begin();
+
+  if (WEBSERVER_ENABLED) {
+    Serial.println("WEBSERVER ENABLED");
+    server.on("/systemreset", handleSystemReset);
+    server.on("/forgetwifi", handleWifiReset);
+    server.on("/configure", handleConfigure);
+    server.onNotFound(handleRoot);
+    server.begin();
+    Serial.println("Server started");
+    String webAddress = "http://" + WiFi.localIP().toString() + ":" + String(WEBSERVER_PORT) + "/";
+    Serial.println("Use this URL : " + webAddress);
+  } else {
+    Serial.println("WEBSERVER DISABLED");
+  }
+
   timeClient.begin();
 }
 
 void loop() {
   timeClient.update();
+  server.handleClient();
+  ArduinoOTA.handle();
+
+  delay(1);
 }
 
-void handleRoot() {
-  digitalWrite(externalLight, 1);
-  server.send(200, "text/plain", "hello from esp8266!");
-  digitalWrite(externalLight, 0);
+void handleSystemReset() {
+  if (!server.authenticate(www_username, www_password)) {
+    return server.requestAuthentication();
+  }
+  Serial.println("Reset System Configuration");
+  if (SPIFFS.remove(CONFIG)) {
+    handleRoot();
+    ESP.restart();
+  }
+}
+
+void handleWifiReset() {
+  if (!server.authenticate(www_username, www_password)) {
+    return server.requestAuthentication();
+  }
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  handleRoot();
+  WiFiManager wifiManager;
+  wifiManager.resetSettings();
+  ESP.restart();
+}
+
+// converts the dBm to a range between 0 and 100%
+int8_t getWifiQuality() {
+  int32_t dbm = WiFi.RSSI();
+  if (dbm <= -100) {
+    return 0;
+  } else if (dbm >= -50) {
+    return 100;
+  } else {
+    return 2 * (dbm + 100);
+  }
+}
+
+void writeSettings() {
+  // Save decoded message to SPIFFS file for playback on power up.
+  File f = SPIFFS.open(CONFIG, "w");
+  if (!f) {
+    Serial.println("File open failed!");
+  } else {
+    Serial.println("Saving settings now...");
+    f.println("www_username=" + String(www_username));
+    f.println("www_password=" + String(www_password));
+  }
+  f.close();
+  readSettings();
+}
+
+void handleUpdateConfigure() {
+  if (!server.authenticate(www_username, www_password)) {
+    return server.requestAuthentication();
+  }
+
+  String temp = server.arg("userid");
+  temp.toCharArray(www_username, sizeof(temp));
+  temp = server.arg("stationpassword");
+  temp.toCharArray(www_password, sizeof(temp));
+  OTA_Password = server.arg("otapassword");
+
+  writeSettings();
+  handleConfigureNoPassword();
+}
+
+void readSettings() {
+  if (SPIFFS.exists(CONFIG) == false) {
+    Serial.println("Settings File does not yet exists.");
+    writeSettings();
+    return;
+  }
+  File fr = SPIFFS.open(CONFIG, "r");
+  String line;
+  while (fr.available()) {
+    line = fr.readStringUntil('\n');
+
+    if (line.indexOf("www_username=") >= 0) {
+      String temp = line.substring(line.lastIndexOf("www_username=") + 13);
+      temp.trim();
+      temp.toCharArray(www_username, sizeof(temp));
+      Serial.println("www_username=" + String(www_username));
+    }
+    if (line.indexOf("www_password=") >= 0) {
+      String temp = line.substring(line.lastIndexOf("www_password=") + 13);
+      temp.trim();
+      temp.toCharArray(www_password, sizeof(temp));
+      Serial.println("www_password=" + String(www_password));
+    }
+  }
+  fr.close();
 }
 
 void handleNotFound() {
-  digitalWrite(externalLight, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URL in the request
+}
+
+void handleRoot() {
+  String form = parseHomePage();
+  server.send(200, "text/html", form);
+}
+
+void handleConfigure() {
+  if (!server.authenticate(www_username, www_password)) {
+    return server.requestAuthentication();
   }
-  server.send(404, "text/plain", message);
-  digitalWrite(externalLight, 0);
+  String form = parseConfigurePage();
+  form.replace("%USERID%", www_username);
+  form.replace("%STATIONPASSWORD%", www_password);
+  form.replace("%OTAPASSWORD%", OTA_Password);
+
+  server.send(200, "text/html", form);
+}
+
+void handleConfigureNoPassword() {
+  String form = parseConfigurePage();
+  form.replace("%USERID%", www_username);
+  form.replace("%STATIONPASSWORD%", www_password);
+  form.replace("%OTAPASSWORD%", OTA_Password);
+
+  server.send(200, "text/html", form);
 }
